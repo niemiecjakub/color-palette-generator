76151a20fd30687b2b699b26a50fcc2a8d68ad73
niemiecjakub <kniemiec3@gmail.com>
2024-03-21 23:47:21 +0100

    file replacement

--- index.js
+++ index.js
@@ -1,60 +1,54 @@
 const randomHexColorCode = () => {
     let n = (Math.random() * 0xfffff * 1000000).toString(16);
-    return '#' + n.slice(0, 6);
+    return "#" + n.slice(0, 6);
 };
-
 
 const setColor = (element) => {
     const color = randomHexColorCode();
     element.style.backgroundColor = color;
-    element.querySelector(".color-code").innerHTML = color.toUpperCase();
-}
+};
 
 const getColorPalette = () => {
     const colorContainers = document.getElementsByClassName("color-container");
     for (element of colorContainers) {
-        if (!element.querySelector(".locked")) {
-            setColor(element)
+        setColor(element);
+    }
+};
+
+
+const addColorRows = () => {
+    const screenWidth = window.innerWidth;
+    let nPalettesInRow = Math.floor(screenWidth/150)
+    if(nPalettesInRow > 7){
+        nPalettesInRow = 7
+    }
+    for (let i = 0; i < 10; i++) {
+        var newRow = document.createElement("div");
+        newRow.classList.add("color-row");
+
+        for (var j = 0; j < nPalettesInRow; j++) {
+            var rowContainer = document.createElement("div");
+            rowContainer.classList.add("color-row-container");
+
+            for (var k = 0; k < 5; k++) {
+                var colorContainer = document.createElement("div");
+                colorContainer.classList.add("color-container");
+                rowContainer.appendChild(colorContainer);
+            }
+            newRow.appendChild(rowContainer);
         }
+        var paletteSection = document.getElementById("palette");
+        paletteSection.appendChild(newRow);
     }
-}
+};
 
-const copyToClipboard = (e) => {
-    const copyInfo =document.querySelector(".copy-info");
-    copyInfo.parentElement.classList.add("slide-animation")
-    setTimeout(() => copyInfo.parentElement.classList.remove("slide-animation"), 3000)
-    const parent = e.parentElement;
-    const colorCode = parent.querySelector(".color-code").innerHTML;
-    navigator.clipboard.writeText(colorCode);
-}
-
-const swapColor = (e) => {
-    const parent = e.parentElement;
-    setColor(parent)
-}
-
-const setLock = (e) => {
-    const { classList } = e;
-    if (classList.contains("unlocked")) {
-        classList.remove("unlocked")
-        classList.add("locked")
-        e.src = "resources/locked.svg"
-    } else {
-        classList.remove("locked")
-        classList.add("unlocked")
-        e.src = "resources/unlocked.svg"
-    }
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const palette = document.getElementById("palette");
+    const clone = palette.cloneNode(true);
+    palette.parentNode.replaceChild(clone, palette);
+});
 
 
-document.body.onkeyup = function (e) {
-    if (e.key == " " ||
-        e.code == "Space" ||
-        e.keyCode == 32
-    ) {
-        getColorPalette()
-    }
-}
-
-
+addColorRows();
 getColorPalette();
+setInterval(getColorPalette, 1000);
