57e7be4545e3a240f8daf4c4b61ffd2bbedf0ec8
niemiecjakub <kniemiec3@gmail.com>
2024-03-21 19:52:47 +0100

    initial

---
+++ index.js
@@ -0,0 +1,60 @@
+const randomHexColorCode = () => {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6);
+};
+
+
+const setColor = (element) => {
+    const color = randomHexColorCode();
+    element.style.backgroundColor = color;
+    element.querySelector(".color-code").innerHTML = color.toUpperCase();
+}
+
+const getColorPalette = () => {
+    const colorContainers = document.getElementsByClassName("color-container");
+    for (element of colorContainers) {
+        if (!element.querySelector(".locked")) {
+            setColor(element)
+        }
+    }
+}
+
+const copyToClipboard = (e) => {
+    const copyInfo =document.querySelector(".copy-info");
+    copyInfo.parentElement.classList.add("slide-animation")
+
+    const parent = e.parentElement;
+    const colorCode = parent.querySelector(".color-code").innerHTML;
+    navigator.clipboard.writeText(colorCode);
+}
+
+const swapColor = (e) => {
+    const parent = e.parentElement;
+    setColor(parent)
+}
+
+const setLock = (e) => {
+    const { classList } = e;
+    if (classList.contains("unlocked")) {
+        classList.remove("unlocked")
+        classList.add("locked")
+        e.src = "resources/locked.svg"
+    } else {
+        classList.remove("locked")
+        classList.add("unlocked")
+        e.src = "resources/unlocked.svg"
+    }
+}
+
+
+document.body.onkeyup = function (e) {
+    if (e.key == " " ||
+        e.code == "Space" ||
+        e.keyCode == 32
+    ) {
+        getColorPalette()
+    }
+}
+
+
+getColorPalette();
