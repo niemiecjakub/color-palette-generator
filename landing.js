166aa231202b53d1e9c0d4db82d52a12858c494c
niemiecjakub <kniemiec3@gmail.com>
2024-03-21 22:48:46 +0100

    explore page

--- landing.js
+++ landing.js
@@ -9,11 +9,45 @@ const setColor = (element) => {
 };
 
 const getColorPalette = () => {
-    const colorContainers = document.getElementsByClassName("pc-color-container");
+    const colorContainers = document.getElementsByClassName("color-container");
     for (element of colorContainers) {
         setColor(element);
     }
 };
 
 getColorPalette();
-setInterval(getColorPalette, 1000)
+setInterval(getColorPalette, 1000);
+
+const addColorRows = () => {
+    const screenWidth = window.innerWidth;
+    let nPalettesInRow = Math.floor(screenWidth/150)
+    if(nPalettesInRow > 7){
+        nPalettesInRow = 7
+    }
+    for (let i = 0; i < 10; i++) {
+        var newRow = document.createElement("div");
+        newRow.classList.add("color-row");
+
+        for (var j = 0; j < nPalettesInRow; j++) {
+            var rowContainer = document.createElement("div");
+            rowContainer.classList.add("color-row-container");
+
+            for (var k = 0; k < 5; k++) {
+                var colorContainer = document.createElement("div");
+                colorContainer.classList.add("color-container");
+                rowContainer.appendChild(colorContainer);
+            }
+            newRow.appendChild(rowContainer);
+        }
+        var paletteSection = document.getElementById("palette");
+        paletteSection.appendChild(newRow);
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    const palette = document.getElementById("palette");
+    const clone = palette.cloneNode(true);
+    palette.parentNode.replaceChild(clone, palette);
+});
+
+addColorRows();
